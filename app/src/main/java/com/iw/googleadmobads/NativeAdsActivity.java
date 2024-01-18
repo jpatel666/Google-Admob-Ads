b2648d6631e89a6dbee01c382e31c45e095f0da4
Jimmy <jhpatel392@gmail.com>
2024-01-18 01:04:43 -0800

    Google Admob Ads

---
+++ app/src/main/java/com/iw/googleadmobads/NativeAdsActivity.java
@@ -0,0 +1,43 @@
+package com.iw.googleadmobads;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.ads.nativetemplates.NativeTemplateStyle;
+import com.google.android.ads.nativetemplates.TemplateView;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.nativead.NativeAd;
+
+public class NativeAdsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_native_ads);
+
+        loadNative();
+    }
+
+    void loadNative() {
+
+        MobileAds.initialize(this);
+        AdLoader adLoader = new AdLoader.Builder(this, getString(R.string.native_id))
+                .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
+                    @Override
+                    public void onNativeAdLoaded(NativeAd nativeAd) {
+                        NativeTemplateStyle styles = new
+                                NativeTemplateStyle.Builder().build();
+                        TemplateView template = findViewById(R.id.my_template);
+                        template.setStyles(styles);
+                        template.setNativeAd(nativeAd);
+                    }
+                })
+                .build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+
+    }
+}
--- app/src/main/java/com/iw/googleadmobads/SplashScreenActivity.java
+++ app/src/main/java/com/iw/googleadmobads/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package com.iw.googleadmobads;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
